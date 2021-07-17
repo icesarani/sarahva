c280641b9c2f0d2f91e4fb632dc6dcc54c52c760
Ignacio Cesarani <84433278+icesarani@users.noreply.github.com>
2021-07-17 17:21:42 -0300

    Add files via upload
    Sarah v1.1 All in English

--- sarahva.py
+++ sarahva.py
@@ -11,17 +11,19 @@ listener = sr.Recognizer()
 engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+engine.setProperty('voice', voices[0].id)
 
 namea = 'sarah'
 namey = "Señor Stark"
+cels =["father:+541149770791","adoptado:+5491141673005","monkey:+5491132512401"]
+exe = ['discord.exe','notepad.exe','chrome.exe','pycharm.exe']
 
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-engine.say(f"Buenos días {namey} ya estoy activa por y para usted...")
-print(f"+Buenos días {namey} ya estoy activa por y para usted...")
+engine.say(f"Hi {namey} I am active by and for you...")
+print(f"+Hi {namey} I am active by and for you...")
 engine.runAndWait()
 
 def listen():
@@ -32,154 +34,202 @@ def listen():
     rec = ''
     try:
         with sr.Microphone() as source:
-            print("Escuchando...")
+            print("Listening...")
+            listener.adjust_for_ambient_noise(source, duration=0.2)
             voice = listener.listen(source)
-            rec = listener.recognize_google(voice, language='en')
+            rec = listener.recognize_google(voice)
             rec = rec.lower()
-            print("-Usted dijo: "+rec)
+            print("-You said: "+rec)
     except:
         pass
 
     return rec
 
+def subrun():
+    rec = listen()
+    try:
+        if 'play' in rec:
+            play(rec)
+
+        elif 'triste' in rec:
+            triste()
+
+        elif 'date' in rec:
+            fecha()
+
+        elif 'time' in rec:
+            hora()
+
+        elif 'info' in rec:
+            wiki(rec)
+
+        elif 'search' in rec:
+            buscar(rec)
+
+        elif 'open' in rec:
+            abrir(rec)
+
+        elif 'como se llama dios' in rec:
+            talk("My lord and master is called Ignacio Cesarani")
+            print("+My lord and master is called Ignacio Cesarani")
+
+        elif 'suicide' in rec:
+            suicidio()
+
+        elif 'what is your name' in rec:
+            wiyn()
+
+        elif 'shutdown' in rec:
+            shutdown()
+
+        elif 'send' in rec:
+            send(rec) 
+
+        elif 'exit' in rec:
+            exit()
+            return False
+
+        elif 'do you love me' in rec:
+            amor()
+
+
+        else:
+            print("+Try again")
+            talk("Try again")
+    except:
+        pass
+
+    return True
+
 def run():
-    '''Funcion principal donde se lleva adelante todo el programa'''
+    '''Main function where the whole program is carried out'''
 
     rec = listen()
+    rec = rec.replace('sada', 'sarah')
+    rec = rec.replace('zara', 'sarah')
+
     if namea in rec:
-        talk(f"Digame {namey}, que necesita...")
-        print(f"+Digame {namey}, que necesita...")
-        rec = listen()
-        try:
-            if 'play' in rec:
-                play(rec)
+        talk(f"Tell me {namey}, what do you need?...")
+        print(f"+Tell me {namey}, what do you need?...")
 
-            elif 'triste' in rec:
-                triste()
+        seguir = True
+        cont = 0
+        subrun()
+        while(cont<7)and(seguir==True):
+            seguir = subrun()
+            cont=cont+1
+        print("+I go to invisible mode, I start again with the command  "+namea)
+        talk("I go to invisible mode, I start again with the command  "+namea)
 
-            elif 'date' in rec:
-                fecha()
-
-            elif 'time' in rec:
-                hora()
-
-            elif 'info' in rec:
-                wiki(rec)
-
-            elif 'search' in rec:
-                buscar(rec)
-
-            elif 'como se llama dios' in rec:
-                talk("Mi amo y señor se llama Ignacio Cesarani")
-                print("+Mi amo y señor se llama Ignacio Cesarani")
-
-            elif 'suicide' in rec:
-                suicidio()
-
-            elif 'what is your name' in rec:
-                wiyn()
-
-            elif 'shutdown' in rec:
-                shutdown()
-
-            elif 'send' in rec:
-                send(rec)
-
-            elif 'exit' in rec:
-                exit()
-                return jt==False
-
-
-            else:
-                print("+Vuelva a intentarlo")
-                talk("Vuelva a intentarlo")
-        except:
-            pass
+    elif (rec=='exit'):
+        exit()
+        return jt==False
 
     return jt==True
 
+def abrir(rec):
+    '''Open an application // Usage: one enters "open" and an app that is searched among the list of apps'''
+
+    rec = rec.replace('open', '')
+    app = ''
+    for k in range(len(exe)):
+        aux = exe[k].split('.')
+        if aux[0] in rec:
+            app = aux[0]+'.'+aux[1]
+            break
+
+    os.system("start "+app)
+
+
+
 def fecha():
-    '''Te dice que fecha es // Uso: uno ingresa date y Sarah te dice que dia es hoy'''
+    '''It tells you what date it is // Use: one enters date and Sarah tells you what day it is today'''
 
     dia = datetime.today()
     dia = dia.strftime("%d/%m/%Y")
-    print("+Hoy es " + dia)
-    talk("Hoy es " + dia)
+    print("+Today is " + dia)
+    talk("Today is " + dia)
 
 def hora():
-    '''Te dice que hora es // Uso: uno ingresa time y Sarah te dice que hora es actualmente'''
+    '''It tells you what time it is // Usage: you enter time and Sarah tells you what time it is currently'''
 
     tim = datetime.now()
     tim = tim.strftime("%I:%M %p")
-    print("+La hora actual es " + tim)
-    talk("La hora actual es " + tim)
+    print("+The current time is " + tim)
+    talk("The current time is " + tim)
 
 def exit():
-    '''Deja de funcionar el programa // Uso: uno ingresa exit y el programa se sale de su while infinito de funcion'''
+    '''The program stops working // Usage: one enters exit and the program exits its infinite while function'''
 
-    print("+Nos vemos pronto "+namey)
-    talk("Nos vemos pronto "+namey)
+    print("+See you soon "+namey)
+    talk("See you soon "+namey)
 
 def send(rec):
-    '''Se envia un mensaje por wpp a una persona en especifico /// Uso: uno ingresa "send (nombre)"
-    el nombre definira el destinatario y luego se pedira ingresar
-    que texto desea enviar'''
+    '''
+    A message is sent by wpp to a specific person /// Use: one enters "send (name)"
+    The name will define the recipient and then you will be asked to enter
+    what text do you want to send'''
 
-    print("+Se enviara un mensaje por whatsapp")
-    talk("Se enviara un mensaje por whatsapp")
-    if 'monkey' in rec:
-        cel = "+5491132512401"
-    elif 'adoptado' in rec:
-        cel = "+5491141673005"
-    elif 'papa' in rec:
-        cel = "+541149770791"
+    print("+A message will be sent by whatsapp")
+    talk("A message will be sent by whatsapp")
+
+    cel = ''
+
+    for k in range(len(cels)):
+        aux = cels[k].split(':')
+        if aux[0] in rec:
+            cel = aux[1]
 
     if cel != '':
-        print(f"+Se le enviara el mensaje a {cel} que mensaje desea enviar?")
-        talk(f"Se le enviara el mensaje a {cel} que mensaje desea enviar?")
+        print(f"+The message will be sent to {cel} What message do you want to send?")
+        talk(f"The message will be sent to {cel} What message do you want to send")
         rec = listen()
-        print("Tu mensaje fue: " + rec)
+        print("Your message was: " + rec)
         pw.sendwhatmsg(cel, rec, int(datetime.now().strftime("%H")), int(datetime.now().strftime("%M")) + 1)
+    else:
+        print("+No recipient was found")
+        talk("No recipient was foundo")
 
 def shutdown():
-    '''Apaga su PC // Uso: al instruirle "shutdown" a Sarah se te preguntara si desea apagar su PC, si dice "si" se apagara'''
+    '''Turn off your PC // Usage: when instructing Sarah to "shutdown" she will ask if she wants to shut down her PC, if she says "yes" to shut down'''
 
 
-    print("+Esta seguro de que desea apagar la computadora?")
-    talk("Esta seguro de que desea apagar la computadora?")
+    print("+Are you sure you want to shut down the computer?")
+    talk("Are you sure you want to shut down the computer?")
     rec = listen()
-    if 'si' in rec:
-        print("+Espero verlo dentro de poco "+namey)
-        talk("Espero verlo dentro de poco "+namey)
-        talk("Se apagara su PC")
+    if 'yes' in rec:
+        print("+I hope to see you soon "+namey)
+        talk("I hope to see you soon "+namey)
+        talk("Your PC will shut down")
         time.sleep(2)
         os.system("shutdown /p")
     else:
-        print("+Se ha ancelado el apagado de pc")
-        talk("Se ha cancelado el apagado de pc")
+        print("+PC shutdown has been canceled")
+        talk("PC shutdown has been canceled")
 
 def wiyn():
-    '''Printea su nombre // Uso: preguntar "what is your name" y el bot printeara y usara de talk para decirlo'''
+    '''Print her name // Use: ask "what is your name" and the bot will print and use talk to say it'''
 
-    print("+Mi nombre es " + namea)
-    talk("Mi nombre es " + namea)
+    print("+My name is " + namea)
+    talk("My name is " + namea)
 
 def play(rec):
-    '''Se reproduce un video en yt // Uso: uno ingresa "play (titulo video)"
-    se buscara el video màs acorde a lo ingresado y se reproducira
-    en algun buscador'''
+    '''
+    A video is played in yt // Use: one enters "play (video title)"
+    the video that best matches what was entered will be searched and it will be played
+    in some search engine'''
 
     music = rec.replace('play', '')
-    talk('Reproduciendo' + music)
-    print("+Reproduciendo")
+    talk('Playing' + music)
+    print("+Playing")
     pywhatkit.playonyt(music)
     time.sleep(10)
 
 def suicidio():
-    '''Se hace una broma sobre la autodestruccion del bot'''
+    '''A joke is made about the bot's self-destruction'''
 
-    print("+Me autodestruire en")
-    talk("Me autodestruire en")
+    print("+I will self-destruct in")
+    talk("I will self-destruct in")
     print("+3")
     talk("3")
     time.sleep(1)
@@ -189,8 +239,8 @@ def suicidio():
     print("+1")
     talk("1")
     time.sleep(1)
-    print("+Jaja es broma")
-    talk("Ja ja es broma")
+    print("+Haha just kidding")
+    talk("Haha just kidding")
 
 def triste():
     '''Momento sad para cuando estas triste'''
@@ -211,21 +261,25 @@ def triste():
     time.sleep(2)
     print("+bueno sigamos")
 
-def buscat(rec):
-    '''Busca en google lo ingresado // Uso: uno ingresa "search (busqueda deseada)" y se abrira
-    una pagina de google donde se busca lo ingresado'''
+def buscar(rec):
+    '''Search in google what was entered // Use: one enters "search (desired search)" and it will open
+    a google page where the entered is searched'''
 
     buscar = rec.replace('search', '')
     pywhatkit.search(buscar)
     time.sleep(10)
 
 def wiki(rec):
-    '''Explica informacion sobre lo ingresado // Uso: se busca y reproduce lo ingresado en wikipedia'''
+    '''Explain information about what was entered // Use: what is entered in wikipedia is searched and reproduced'''
 
     order = rec.replace('info', '')
     info = wikipedia.summary(order, 1)
     print(info)
     talk(info)
+
+def amor():
+    print("+I do love you "+namey)
+    talk("I do love you "+namey)
 
 jt = True
 while jt == True:
